ab2c8086b73b53eaeacfe0e623496423d22159dd
Sergey Papilov <papilov.serhz@gmail.com>
2025-03-08 03:25:48 +0300

    HW №4 Функция дозаписи в TXT по классу

--- txt_file_handler.py
+++ txt_file_handler.py
@@ -2,3 +2,7 @@ class TxtFileHandler:
     def write_file(self, file_path: str, data: str):
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(data)
+
+    def append_file(self, file_path: str, data: str):
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write(data)
