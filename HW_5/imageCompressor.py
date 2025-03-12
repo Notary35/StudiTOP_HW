44be341419e7d5652fdcc5c2286e0c947c4eeb0c
Papilov Sergey <papilov.serhz@gmail.com>
2025-03-13 01:15:43 +0300

    HW №5 Создал 2 метода для обработки 1 файла или всеё директории с поддиректориями

--- HW_5/imageCompressor.py
+++ HW_5/imageCompressor.py
@@ -19,3 +19,23 @@ class ImageCompressor:
         Ничего не возвращает
         """
         self.__quality = quality
+        
+    def compress_image(self, input_path: str, output_path: str) -> None:
+        """
+        Сжимает изображение и сохраняет его в формате HEIF.
+        """
+        with Image.open(input_path) as img:
+            img.save(output_path, "HEIF", quality = self.__quality)
+            print(f"Сжато: {input_path} -> {output_path}")
+            
+    def process_directory(self, directory: str) -> None:
+        """
+        Обрабатывает все изображения в указанной директории и её поддиректориях.
+        """
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith(self.supported_formats):
+                    input_path = os.path.join(root, file)
+                    output_path = os.path.splitext(input_path)[0] + '.heic'
+                    self.compress_image(input_path, output_path)
+    
