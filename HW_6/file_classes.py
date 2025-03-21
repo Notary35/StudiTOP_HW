0cc789191d2d1776fc017b08f51ec9393cb25de5
Papilov Sergey <papilov.serhz@gmail.com>
2025-03-21 20:26:27 +0300

    HW №6 Создал абстрактный класс для работы с TXT файлом

--- HW_6/file_classes.py
+++ HW_6/file_classes.py
@@ -23,3 +23,47 @@ class AbstractFile(ABC):
         pass
 
 
+class TxtFile(AbstractFile):
+
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+    def write_file(self, data: str, encoding: str = "utf-8"):
+        """
+        Метод для записи данных в TXT файл.
+        Записывает строку
+        """
+        try:
+            with open(self.file_path, "w", encoding=encoding) as file:
+                if not isinstance(data, str):
+                    raise TypeError("TXT файл должен содержать строку")
+                file.write(data)
+        except Exception as e:
+            print(f"Произошла ошибка при записи файла: {e}")
+
+    def append_file(self, data: str, encoding: str = "utf-8"):
+        """
+        Метод для дозаписи данных в TXT файл.
+        Дозаписывает строку
+        """
+        try:
+            with open(self.file_path, "a", encoding=encoding) as file:
+                if not isinstance(data, str):
+                    raise TypeError("TXT файл должен содержать строку")
+                file.write(data)
+        except Exception as e:
+            print(f"Произошла ошибка при записи файла: {e}")
+
+    def read_file(self, encoding="utf-8"):
+        """
+        Метод для чтения данных из TXT файла.
+        """
+        try:
+            with open(self.file_path, "r", encoding=encoding) as file:
+                data = file.read()
+            return data
+        except FileNotFoundError:
+            return ""
+        except Exception as e:
+            print(f"Произошла ошибка при чтении файла: {e}")
+            return ""
