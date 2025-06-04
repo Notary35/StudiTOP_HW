3c169fe64752381fae994b8f3e27268ab57be4b1
Papilov Sergey <papilov.serhz@gmail.com>
2025-06-04 23:37:20 +0300

    HW №10 Создал сет спец символов и декоратор

---
+++ HW_10/hw_10.py
@@ -0,0 +1,32 @@
+"""
+# Домашнее задание №10📃
+## Часть 1: Декоратор для валидации пароля
+"""
+from typing import Callable
+
+special_symbols_set = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '~', '`', '{', '}', '[', ']', '|', '\\', ';', ':', "'", '"', '<', '>', ',', '.', '?', '/'}
+
+user_password = str(input("Создайте пароль: "))
+
+
+def password_checker(length: int = 8) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        def wrapper(password: str):
+            if len(password) < length:
+                print("Пароль должен быть не менее 8 символов")
+            if set(password) & special_symbols_set == set():
+                special_symbols_list = list(special_symbols_set)
+                print(f"Пароль должен содержать хотя-бы один спец-символ: ({', '.join(special_symbols_list[:5])}) и т.д.")
+            if not any (simbol.isalpha() for simbol in password):
+                print("Пароль должен содержать буквенные символы")
+                if not any (simbol.isupper() for simbol in password):
+                    print("Пароль должен содержать хотя-бы одну заглавную букву")
+                if not any (simbol.islower() for simbol in password):
+                    print("Пароль должен содержать хотя-бы одну строчную букву")
+            if not any(simbol.isdigit() for simbol in password):
+                print("Пароль должен содержать хотя-бы одну цифру")
+            else:
+                return func(password)
+        return wrapper
+    return decorator
+
---
+++ HW_8/Game_city_2.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass, field
+from typing import List, Dict, Any
+import json
+from random import choice
+from pprint import pprint
+
