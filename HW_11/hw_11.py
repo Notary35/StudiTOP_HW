faaa63b7be7922b637b5bd33630e3c8eac56c273
Papilov Sergey <papilov.serhz@gmail.com>
2025-06-16 22:28:39 +0300

    HW №11 Итератор с инициализацией и валидатором для обработки и фильтрации списка городов

--- HW_11/hw_11.py
+++ HW_11/hw_11.py
@@ -12,3 +12,18 @@ class City:
     population: int
     subject: str
 
+class CitiesIterator:
+    def __init__(self, cities: List[Dict[str, Any]]):
+        self._original_data = cities
+        self._min_population: Optional[int] = None
+        self._sorted: Optional[str] = None
+        self._reverse: Optional[bool] = None
+        self._index = 0
+
+    def _validate_city_dict(self, city_dict: Dict[str, Any]):
+        required_fields = ["name", "district", "population", "subject", "coords"]
+        for field in required_fields:
+            if field not in city_dict:
+                raise ValueError(f"Отсутствует пункт {field} в городе {city_dict['name']}")
+        if 'lat' not in city_dict['coords'] or 'lon' not in city_dict['coords']:
+            raise ValueError(f"В пункте 'coords' должны быть подпункты 'lat' и 'lon': {city_dict['name']}")
